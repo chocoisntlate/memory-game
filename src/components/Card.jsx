7d78f1036ebdb262d18ff867a142f249923e16ba
chocoisntlate <130369559+chocoisntlate@users.noreply.github.com>
2025-08-27 13:51:02 +0700

    using mock data instead

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,21 @@
-function Card({name, src}) {
+export function Card({name, url}) {
+
     return (
         <div className="card">
-            <img src={src}></img>
+            <img src={url}></img>
             <span>{name}</span>
         </div>
     )
+}
+
+export function CardDisplay({cardsList}) {
+    return (
+        <div className="card-display">
+            {cardsList.map((card) => {
+                const name = card.value + " OF " + card.suit;
+                return <Card name={name} url={card.image} key={card.code}></Card>
+            })}
+        </div>
+    )   
+
 }
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
+import { CardDisplay } from "./Card";
+import mockData from "../mockData.json"
 
 export function Game() {
     const [highScore, setHighScore] = useState(0)
     const [currentScore, setCurrentScore] = useState(0)
     const [trackedCardsList, setTrackedCardsList] = useState([])
-    const [cardsList, setCardsList] = useState([])
+    const [cardsList, setCardsList] = useState([1])
 
     useEffect(() => {
         async function loadCards() {
@@ -17,15 +19,20 @@ export function Game() {
             if (data.success) {
                 setCardsList(data.cards);
                 console.log(data.cards);
+                // console.log(JSON.stringify(data.cards, null, 2)) 
             } // TODO: err hand
 
         }
-        loadCards()
+        // UNCOMMENT LATER: loadCards()
+        
+        // For testing
+        setCardsList(mockData)
     }, [])
 
     return (
         <>
             <ScoreBoard currentScore={currentScore} highScore={highScore}/>
+            <CardDisplay cardsList={cardsList}/>
         </>
 
     )
